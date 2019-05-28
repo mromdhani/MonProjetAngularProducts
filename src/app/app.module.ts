4398946804961dbd1064243bbd590df9f6af98ea
Mohamed Romdhani <student@bt-training.be>
2019-05-28 15:48:42 +0200

    Commit de la fonctionnalité de Routage (simplifiée)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,34 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {RouterModule, Route} from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { ProductsComponent } from './components/products/products.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { WelcomeComponent } from './components/welcome/welcome.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
+
+const routingTable: Route[] = [      // Table de routage
+    {path: '', redirectTo: 'welcome', pathMatch: 'full'},
+    {path: 'welcome', component : WelcomeComponent},
+    {path: 'products', component : ProductsComponent},
+    {path: '**', component : NotFoundComponent},
+
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     ProductsComponent,
-    FooterComponent
+    FooterComponent,
+    WelcomeComponent,
+    NotFoundComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routingTable)
   ],
   providers: [],
   bootstrap: [AppComponent]
